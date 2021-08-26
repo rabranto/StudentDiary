b2547f731cd66ca0b79f8c8b7d4ce807da48064d
Radosław <57417179+rabranto@users.noreply.github.com>
2021-08-26 15:09:09 +0200

    Add Diary class and methods

---
+++ StudentDiary/StudentDiary/Diary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        public Diary()
+        {
+            grades = new List<double>();
+        }
+
+        private List<double> grades;
+
+        public void AddGrades(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowGrades()
+        {
+            foreach (var grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+
+        public double CalculateAverage()
+        {
+            double sum = 0;
+            foreach (var grade in grades)
+            {
+                sum += grade;
+            }
+
+            return sum / grades.Count();
+        }
+
+        public void GetBestGrade()
+        {
+            Console.WriteLine(grades.Max());
+        }
+        public void GetWorstGrade()
+        {
+            Console.WriteLine(grades.Min());
+        }
+    }
+}
---
+++ StudentDiary/StudentDiary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StudentDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new();
+
+            diary.AddGrades(5);
+            diary.AddGrades(3.5);
+            diary.AddGrades(4);
+            diary.AddGrades(6);
+            diary.AddGrades(1);
+
+            diary.GetBestGrade();
+            diary.GetWorstGrade();
+            Console.ReadKey();
+        }
+    }
+}
